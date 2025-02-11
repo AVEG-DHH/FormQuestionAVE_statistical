134dd31a761fbfe7a8f68d61f093d26627ae1dfb
MTruong2000 <truongdev2000@gmail.com>
2025-02-11 14:16:05 +0700

    update code sidebar

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -2,18 +2,21 @@ import Navbar from '../navbar';
 import Sidebar from '../sidebar';
 
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 
 import './style.scss';
 
 const Layout = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
     return (
         <>
-            <Navbar />
+            <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
             <div className="layout-container">
-                <div className="layout-container-sidebar">
+                <div className={`layout-container-sidebar ${isSidebarOpen ? 'open-sidebar' : 'close-sidebar'}`}>
                     <Sidebar />
                 </div>
-                <div className="layout-container-content">
+                <div className={`layout-container-content ${isSidebarOpen ? '' : 'full-content'}`} >
                     <Outlet />
                 </div>
             </div>
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,13 +1,18 @@
 import { CgMoveLeft, CgMoveRight } from "react-icons/cg";
 import './style.scss';
 
-const Navbar = () => {
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
         <div className="navbar-container">
             <div className="navbar-container-logo">Natural Cleansing</div>
             <div className="navbar-container-icon">
-                <CgMoveLeft />
-                {/* <CgMoveRight /> */}
+                {
+                    isSidebarOpen ? <CgMoveLeft onClick={toggleSidebar} /> : <CgMoveRight onClick={toggleSidebar} />
+                }
             </div>
         </div>
     );
